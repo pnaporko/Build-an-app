31ef0c0c1f771047a067edcdd13ea3a1d7c30afe
pavel <361cf7bc@opayq.com>
2016-10-12 15:21:44 -0400

    com

--- Build an app/MainWindow.xaml.cs
+++ Build an app/MainWindow.xaml.cs
@@ -38,7 +38,13 @@ namespace Build_an_app
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-
+            int x = 5;
+            if (x==10)
+            { myLabel.Text = "x must be 10"; }
+            else
+            {
+                myLabel.Text = "x isn't 10";
+            }
         }
 
         private void button3_Click(object sender, RoutedEventArgs e)
